7b9e0accfa297a6b5a15d6acc3573c6d5ff842bb
sunnysails <sunnysails@qq.com>
2016-12-05 16:37:22 +0800

    创建

---
+++ src/main/java/com/kaishangit/web/AjaxServlet.java
@@ -0,0 +1,51 @@
+package com.kaishangit.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by sunny on 2016/12/5.
+ */
+@WebServlet("/ajax")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+
+        name = new String(name.getBytes("ISO8859-1"),"UTF-8");
+
+        System.out.println("hello,Ajax>>>>" + name);
+        PrintWriter out = resp.getWriter();
+        out.print("Hello,World");
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        resp.setCharacterEncoding("UTF-8");
+
+        resp.setContentType("text/plain;charset=UTF-8");
+        String name = req.getParameter("name");
+
+        PrintWriter out = resp.getWriter();
+
+        if ("tom".equals(name)) {
+            System.out.println("账号被占用");
+            out.print("不能用");
+        } else {
+            System.out.println("账号可以使用");
+            out.print("能用");
+        }
+        System.out.println("hello,Ajax.....dopost....." + name);
+        out.flush();
+        out.close();
+    }
+}
